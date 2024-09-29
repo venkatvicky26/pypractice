c16e727e19e8d87c48eaad13f2498557804d6fb3
venkatvicky26 <vcvenkatcool@gmail.com>
2024-09-29 16:23:08 +0530

    created casting & user input.py

---
+++ casting & user input.py
@@ -0,0 +1,13 @@
+# casting = converting one data type into another data type
+
+a=int('10')
+b=int("20")
+print(a+b)
+
+# user input 
+
+print('enter value for a')
+a=int(input())             # input() prompts for user input at this line
+print('enter value for b')
+b=int(input())
+print('addition of a & b =',a+b)
