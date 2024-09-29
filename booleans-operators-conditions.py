3a86502c4d553cb3b41c660b9c519cbf3b1f9fa9
venkatvicky26 <vcvenkatcool@gmail.com>
2024-09-29 19:26:13 +0530

    added comments for explanation in bools-ops-conds file

--- booleans-operators-conditions.py
+++ booleans-operators-conditions.py
@@ -1,28 +1,31 @@
-#Booleans, Arithmetic, Conditional, Logical operators
+#Booleans - True/False
+#Arithmetic - +,-,/,*,%
+#Conditional - <,>,==,<>or=!
+#Logical operators - And, or, not
+#If-Else, Multiple IFs, If-Elif-Else
 
-#Enter a number to check if it is a even number
+#CHECK IF A NUMBER IS PRIME
 
 #If-Else
 
 #print('Enter a number to check if it is a even number: ')
-number=int(input('Enter a number to check if it is a even number: '))  # Line number 7 - within the input function
+number=int(input('Enter a number to check if it is a even number: '))  # Line number 11 - within the input function
 
 # print(number/2,type(number/2))                                   # Result of division is float even though it is divisible
 
 DataType_Result=number%2==0                                        # % - modulus - gives the remainder
                                                                    # DataType_Result contains a value of boolean data type
-                                                                   
+
 #print(2==2)                                                       # eg for getting boolean result from print function
 print(DataType_Result)
 
-if(str(DataType_Result)=='True'):                                  # == one of the conditional operators
-# str converts the data type to string
+if(str(DataType_Result)=='True'):                                  # str converts the data type to string
     print('It is a even number')
 else:
     print('It is a odd number')
 
 
-#Enter a number to check if it is divisible by both 3 and 5:
+#CHECK DIVISIBILTY OF NUMBER BY 3 AND 5
 
 # Working of Multiple If conditions
 
