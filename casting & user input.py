fdedaa67a655a5610652d140c3b83f2868b7cead
venkatvicky26 <vcvenkatcool@gmail.com>
2024-09-29 17:24:56 +0530

    added user input excercise

--- casting & user input.py
+++ casting & user input.py
@@ -11,3 +11,21 @@ a=int(input())             # input() prompts for user input at this line
 print('enter value for b')
 b=int(input())
 print('addition of a & b =',a+b)
+
+# user input & casting exercise
+# input - Name, Department, Score
+# output - 
+# My name is venkat
+# My department is Mechanical_BI
+# My score is 7.8/10
+
+print('Enter you name','\n','Name:')
+Name=input()
+print('Enter you Department','\n','Department:')
+Department=input()
+print('Enter you Score','\n','Score:')
+Score=int(input())
+
+print('My name is ', Name)
+print('My department is ',Department)
+print('My score is ',Score/10,'/10')
