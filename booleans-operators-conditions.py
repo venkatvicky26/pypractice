d674e290dfd17cb230b47de0277d418d682068c3
venkatvicky26 <vcvenkatcool@gmail.com>
2024-09-29 19:17:03 +0530

    added bools-ops-conds file

---
+++ booleans-operators-conditions.py
@@ -0,0 +1,43 @@
+#Booleans, Arithmetic, Conditional, Logical operators
+
+#Enter a number to check if it is a even number
+
+#If-Else
+
+#print('Enter a number to check if it is a even number: ')
+number=int(input('Enter a number to check if it is a even number: '))  # Line number 7 - within the input function
+
+# print(number/2,type(number/2))                                   # Result of division is float even though it is divisible
+
+DataType_Result=number%2==0                                        # % - modulus - gives the remainder
+                                                                   # DataType_Result contains a value of boolean data type
+                                                                   
+#print(2==2)                                                       # eg for getting boolean result from print function
+print(DataType_Result)
+
+if(str(DataType_Result)=='True'):                                  # == one of the conditional operators
+# str converts the data type to string
+    print('It is a even number')
+else:
+    print('It is a odd number')
+
+
+#Enter a number to check if it is divisible by both 3 and 5:
+
+# Working of Multiple If conditions
+
+
+
+
+#If-Elif-Else
+
+number=int(input('Enter a number to check if it is divisible by both 3 and 5: '))
+
+n3=number%3==0
+n5=number%3==0
+
+if(n3==True and n5==True):
+    print(number,' is divisible by both 3 & 5')
+else:
+    print(number,' is not divisible by both 3 & 5')
+
--- start.py
+++ start.py
@@ -4,4 +4,4 @@ print("Hello, I've begun")
 a=2
 b=6
 c='step by step, you will reach'
-print(a+b,'\t',type(a),'\n',type(c),'\n',c)
+print(a+b,'\t',type(a),'\n',type(c),'\n',c)             # type function gives you the data type
